b084708f6480ce80f7085a8a747592d0d1526f8b
abhi_m <abhishekm@qualitykiosk.com>
2017-08-09 12:49:38 +0700

    2nd Push

--- src/com/fresh/FirstLaunch.java
+++ src/com/fresh/FirstLaunch.java
@@ -4,7 +4,8 @@ public class FirstLaunch {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Printing First Line..!!");
+		System.out.println("Printing 1st Line..!!");
+		System.out.println("Printing 2nd Line..!!");
 		
 	}
 
